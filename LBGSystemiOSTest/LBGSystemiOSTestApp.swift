d4ded188b32d25e457169bf1ea23124dd378c210
Harshit <harshit.2151.dev@gmail.com>
2026-05-05 12:12:41 +0530

    Initial Commit

---
+++ LBGSystemiOSTest/LBGSystemiOSTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LBGSystemiOSTestApp.swift
+//  LBGSystemiOSTest
+//
+//  Created by Harshit Kumar on 05/05/26.
+//
+
+import SwiftUI
+
+@main
+struct LBGSystemiOSTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
